8fa2baec3cf3721469b2849de26f3b61f50e8445
yelneedi <shyelneedi@gmail.com>
2019-04-28 23:34:54 +0000

    new file:   and_operator.sh new file:   or_operator.sh

---
+++ and_operator.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Please enter a number"
+read TEMPNUM
+
+if [ $TEMPNUM -eq "1" ] 2>/dev/null && [ $TEMPNUM -eq "2" ] 2>/dev/null && [ $TEMPNUM -eq "3" ]  2>/dev/null;
+then
+echo " i am odd number"
+else 
+echo " i am even or charecter"
+fi 
